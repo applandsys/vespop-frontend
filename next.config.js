fd79dc764e04c9c308023367d642f1daf7fe0ba8
applandsys <applandsys@gmail.com>
2026-02-16 02:14:54 +0600

    Image permission domain

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-// next.config.js
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost','bdhoms.com','backend.bdhoms.com'], // Allow images from localhost
+        domains: ['localhost','vespop.com','app.vespop.com'],
     },
     eslint: {
         ignoreDuringBuilds: true,
