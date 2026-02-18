4608b66c719d9344a9ba9cbaaaad4130c70f23d1
applandsys <applandsys@gmail.com>
2026-02-19 00:59:48 +0600

    Small tuning

--- src/components/ecommerce/CategoryCarousel.jsx
+++ src/components/ecommerce/CategoryCarousel.jsx
@@ -34,7 +34,7 @@ export default function CategoryCarousel() {
 
     return (
         <section className="max-w-7xl mx-auto px-4 py-10">
-            <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+            <div className="grid grid-cols-2 lg:grid-cols-4 gap-2">
                 {categories
                     .filter(item => item.hasLocation === true)
                     .map((item, index) => (
