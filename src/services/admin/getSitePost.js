b6c5e067d2f1d909459eda1148e2d600aa6f82cb
applandsys <applandsys@gmail.com>
2026-02-22 01:00:21 +0600

    Update post frontend

--- src/services/admin/getSitePost.js
+++ src/services/admin/getSitePost.js
@@ -8,9 +8,23 @@ export const fetchSitePostBySlug = async (slug) => {
 };
 
 export const fetchSitePostById = async (postId) => {
-    const res = await fetch(`${config.apiBaseUrl}/site-post/post/${postId}`);
-    if (!res.ok) throw new Error('Failed to fetch Post');
-    const {data} = await res.json();
-    return data;
+    const res = await fetch(`${config.apiBaseUrl}/admin/site-post/single/${postId}`, {
+        cache: "no-store",
+    });
+
+    if (!res.ok) throw new Error("Failed to fetch");
+
+    return res.json();
+
+};
+
+export const fetchSitePosts = async () => {
+    const res = await fetch(`${config.apiBaseUrl}/admin/site-post`, {
+        cache: "no-store",
+    });
+
+    if (!res.ok) throw new Error("Failed to fetch");
+
+    return res.json();
 };
 
