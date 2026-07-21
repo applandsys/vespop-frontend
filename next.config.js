a9926f22ea55cf5e3fee5f1a97e902093195f20e
Tarique Mosharraf <applandsys@gmail.com>
2026-07-22 00:10:26 +0600

    Init: config fixed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 const nextConfig = {
     images: {
         unoptimized: true,
-        domains: ['localhost','softexpertzone.com','dbarib.softexpertzone.com','94064959ff2f2e1cdff06054b1607835.r2.cloudflarestorage.com','cloudflarestorage.com','240588ae832d6c9bd3565e426bb7e224.r2.cloudflarestorage.com'],
+        domains: ['localhost','vespop.com','api.vespop.com','94064959ff2f2e1cdff06054b1607835.r2.cloudflarestorage.com','cloudflarestorage.com','94064959ff2f2e1cdff06054b1607835.r2.cloudflarestorage.com'],
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'pub-e9c628df12cb482382bd87102a6f71f6.r2.dev',
+                hostname: 'pub-c5509f5d73d4427f894c7c087f54821c.r2.dev',
                 pathname: '/**',
             },
         ],
