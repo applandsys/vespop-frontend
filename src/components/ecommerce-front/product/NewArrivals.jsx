24ec201f0657560c878d776076e3ca8fe583c975
Tarique Mosharraf <applandsys@gmail.com>
2026-07-24 00:29:47 +0600

    Fix: new arrival item gap

--- src/components/ecommerce-front/product/NewArrivals.jsx
+++ src/components/ecommerce-front/product/NewArrivals.jsx
@@ -15,7 +15,7 @@ const NewArrivals = () => {
 
     return (
         <>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 ">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
                 {trendingProduct && trendingProduct.map((product) => (
                     <ProductGridCard key={product.id || product.slug} product={product} />
                 ))}
